136b38147f92433ff1c4293976ccb738cf6307d4
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-04-01 14:25:22 -0500

    Add Google provider

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,23 @@
 import 'twin.macro'
 import Header from 'next/head'
+import { signIn, useSession } from 'next-auth/client'
+
 function Login() {
+  const [session, loading] = useSession()
+
+  if (!loading) {
+    console.log(session)
+  }
+
   return (
     <div tw="relative mx-auto space-y-16 max-w-max top-24">
       <header>
         <h1 tw="bl-text-5xl text-center">Log in</h1>
       </header>
       <main tw="flex flex-col space-y-8">
-        <LoginProviderButton>Continue with Google</LoginProviderButton>
+        <LoginProviderButton onClick={() => signIn('google')}>
+          Continue with Google
+        </LoginProviderButton>
         <LoginProviderButton>Continue with Microsoft</LoginProviderButton>
         <hr tw="w-full h-0 border-t-2 border-lichen-green-200" />
         <EmailLogin />
