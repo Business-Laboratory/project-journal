8c98aa619aff6342353098a580c0629a80d91f36
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-04-06 15:43:49 -0500

    Fix main layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,40 +6,38 @@ import { signIn } from 'next-auth/client'
 const callbackUrl = `${process.env.NEXT_PUBLIC_VERCEL_URL}/`
 function Login() {
   return (
-    <div tw="relative mx-auto space-y-16 max-w-max top-24">
-      <header>
-        <h1 tw="bl-text-5xl text-center">Log in</h1>
-      </header>
-      <main tw="flex flex-col space-y-8">
-        <LoginProviderButton
-          onClick={() => {
-            signIn('google', { callbackUrl })
-          }}
-        >
-          Continue with Google
-        </LoginProviderButton>
-        <LoginProviderButton
-          onClick={() => {
-            signIn('azure-ad-b2c', { callbackUrl })
-          }}
-        >
-          Continue with Microsoft
-        </LoginProviderButton>
-        <hr tw="w-full h-0 border-t-2 border-lichen-green-200" />
-        <EmailLogin />
-      </main>
-    </div>
+    <>
+      <Header>
+        <title>Login | Project Journal</title>
+      </Header>
+      <div tw="mx-auto space-y-16 max-w-max mt-10">
+        <header>
+          <h1 tw="bl-text-4xl text-center">Log in</h1>
+        </header>
+        <main tw="flex flex-col mt-9 space-y-8">
+          <LoginProviderButton
+            onClick={() => {
+              signIn('google', { callbackUrl })
+            }}
+          >
+            Continue with google
+          </LoginProviderButton>
+          <LoginProviderButton
+            onClick={() => {
+              signIn('azure-ad-b2c', { callbackUrl })
+            }}
+          >
+            continue with microsoft
+          </LoginProviderButton>
+          <hr tw="w-full h-0 border-t-2 border-lichen-green-200" />
+          <EmailLogin />
+        </main>
+      </div>
+    </>
   )
 }
 
-function LoginProviderButton(props: React.ComponentPropsWithoutRef<'button'>) {
-  return (
-    <button
-      tw="py-2 px-8 border-2 border-copper-400 bl-text-lg hover:(ring-1 ring-copper-300) focus:(outline-none ring-2 ring-copper-300)"
-      {...props}
-    />
-  )
-}
+export default Login
 
 function EmailLogin() {
   const [email, setEmail] = useState('')
@@ -69,11 +67,6 @@ function EmailLogin() {
         You will be sent a link to a password-free sign in.
       </p>
       <LoginProviderButton
-        css={[
-          disabled
-            ? tw`bg-gray-red-200 text-gray-yellow-500 hover:(ring-0) focus:(ring-0)`
-            : null,
-        ]}
         disabled={disabled}
         onClick={async (e) => {
           e.preventDefault()
@@ -89,10 +82,10 @@ function EmailLogin() {
         }}
         type="submit"
       >
-        {loginState === 'loggingIn' ? 'Sending email...' : 'Sign in with Email'}
+        {loginState === 'loggingIn' ? 'sending email...' : 'sign in with email'}
       </LoginProviderButton>
       {loginState === 'error' ? (
-        <p tw="bl-text-sm text-matisse-red-300 max-w-fit" role="alert">
+        <p tw="bl-text-sm text-matisse-red-200 max-w-fit" role="alert">
           Something went wrong, please try again. If the problem persists you
           can email help@business-laboratory.com
         </p>
@@ -101,26 +94,26 @@ function EmailLogin() {
   )
 }
 
+function LoginProviderButton({
+  disabled,
+  ...props
+}: React.ComponentPropsWithoutRef<'button'>) {
+  return (
+    <button
+      css={[
+        tw`py-2 px-8 border-2 border-copper-300 bl-text-lg uppercase hover:(ring-1 ring-copper-300) focus:(outline-none ring-2 ring-copper-300)`,
+        disabled
+          ? tw`bg-gray-red-200 text-gray-yellow-500 hover:(ring-0) focus:(ring-0)`
+          : null,
+      ]}
+      disabled={disabled}
+      {...props}
+    />
+  )
+}
+
 // taken from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 function validateEmail(email: string) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return re.test(String(email).toLowerCase())
 }
-
-// TODO: move all common page layout elements to either a shared component or `pages/_app.tsx`
-function PageLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <Header>
-        <title>Login | Project Journal</title>
-      </Header>
-      <div tw="bg-gray-yellow-100 text-gray-yellow-600 h-screen">
-        {children}
-      </div>
-    </>
-  )
-}
-
-Login.PageLayout = PageLayout
-
-export default Login
