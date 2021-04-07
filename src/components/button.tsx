986f8297a454ca4d19280b6e90172dfa25a0aad2
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-04-07 13:22:00 -0500

    Style login page (#40)
    * Fix main layout
    
    * Clean up styling of buttons and inputs
    
    * Move Button into it's own file in `components`
    
    * Add hover and focus styles
    
    * Fix button padding and dashed border
    
    * Fix width
    
    * Change hover and focus styles
    
    * Fix button text and hr border width

---
+++ src/components/button.tsx
@@ -0,0 +1,22 @@
+import tw from 'twin.macro'
+
+export { Button }
+export type { ButtonProps }
+
+type ButtonProps = React.ComponentPropsWithoutRef<'button'>
+
+// TODO: Add variant for "danger" button
+function Button({ disabled, ...props }: ButtonProps) {
+  return (
+    <button
+      css={[
+        tw`px-2 py-1 uppercase border-2 border-copper-300 bl-text-lg`,
+        disabled
+          ? tw`bg-gray-yellow-300 bg-opacity-60 text-gray-yellow-600 text-opacity-60`
+          : null,
+      ]}
+      disabled={disabled}
+      {...props}
+    />
+  )
+}
