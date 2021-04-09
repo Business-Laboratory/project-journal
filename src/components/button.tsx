ea50558fa5f8ab504cba635aa7e285167468a1ec
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-04-09 14:58:58 -0500

    Fix disabled button issues

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -11,7 +11,9 @@ function Button({ disabled, ...props }: ButtonProps) {
     <button
       css={[
         tw`px-2 py-1 uppercase border-2 border-copper-300 bl-text-lg`,
-        disabled ? tw`bg-gray-yellow-300 bg-opacity-60 text-opacity-60` : null,
+        disabled
+          ? tw`cursor-not-allowed bg-gray-yellow-300 bg-opacity-60 text-gray-yellow-600 text-opacity-60`
+          : null,
       ]}
       disabled={disabled}
       {...props}
