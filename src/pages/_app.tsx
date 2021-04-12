4e9a0774ee371e054bbfcecdf598cadd9ff72146
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-04-12 16:37:13 -0500

    Test cache control

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,15 +19,15 @@ type ComponentWithPageLayout = {
 const queryClient = new QueryClient()
 
 function App({ Component, pageProps }: AppProps & ComponentWithPageLayout) {
-  const [imageUrl, setImageUrl] = useState('')
-  useEffect(() => {
-    fetch('/api/blob')
-      .then((res) => res.json())
-      .then(({ image }) => {
-        console.log(image)
-        setImageUrl(image)
-      })
-  }, [])
+  // const [imageUrl, setImageUrl] = useState('')
+  // useEffect(() => {
+  //   fetch('/api/blob')
+  //     .then((res) => res.json())
+  //     .then(({ image }) => {
+  //       console.log(image)
+  //       setImageUrl(image)
+  //     })
+  // }, [])
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -36,7 +36,7 @@ function App({ Component, pageProps }: AppProps & ComponentWithPageLayout) {
       <div tw="absolute inset-0 overflow-auto bg-gray-yellow-100 text-gray-yellow-600">
         <AuthProvider>
           <AppBar />
-          {imageUrl ? (
+          {/* {imageUrl ? (
             <div
               tw="relative mt-4 ml-4"
               css={css`
@@ -46,7 +46,23 @@ function App({ Component, pageProps }: AppProps & ComponentWithPageLayout) {
             >
               <Image tw="object-cover" layout="fill" src={imageUrl} alt="" />
             </div>
-          ) : null}
+          ) : null} */}
+
+          <div
+            tw="relative mt-4 ml-4"
+            css={css`
+              width: 170px;
+              height: 210px;
+            `}
+          >
+            <Image
+              tw="object-cover"
+              layout="fill"
+              src="https://projectjournalassets.blob.core.windows.net/test/calumet_optimizer.png?sv=2020-06-12&se=2021-04-12T21%3A40%3A45Z&sr=b&sp=r&sig=gU62IhYL9Dvqq1mEbBG0jmQT1qNCJwCXe%2FCXJhA3uqA%3D&rscc=public%2C%20max-age%3D300%2C%20immutable"
+              alt=""
+            />
+          </div>
+
           <main tw="pt-10">
             {
               // get a page root if one was set
