0b9e7c6960e38e462f34f15f5806776f36ce90f6
tripplybrand <tripplybrand@gmail.com>
2021-04-12 16:26:43 -0400

    Add hover and focus state to button.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -13,7 +13,8 @@ function Button({ disabled, ...props }: ButtonProps) {
         tw`px-2 py-1 uppercase border-2 border-copper-300 bl-text-lg`,
         disabled
           ? tw`cursor-not-allowed bg-gray-yellow-300 bg-opacity-60 text-gray-yellow-600 text-opacity-60`
-          : null,
+          : tw`hover:bg-copper-400 hover:text-gray-yellow-100
+          focus:outline-none focus:ring-2 focus:ring-copper-400`,
       ]}
       disabled={disabled}
       {...props}
