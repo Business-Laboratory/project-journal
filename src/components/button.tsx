b94ee70dc298254da61e0108eb010b28340a680e
tripplybrand <tripplybrand@gmail.com>
2021-04-14 10:34:35 -0400

    Add focus-visible css to button.tsx

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,4 @@
-import tw from 'twin.macro'
+import tw, { css } from 'twin.macro'
 
 export { Button }
 export type { ButtonProps }
@@ -7,14 +7,29 @@ type ButtonProps = React.ComponentPropsWithoutRef<'button'>
 
 // TODO: Add variant for "danger" button
 function Button({ disabled, ...props }: ButtonProps) {
+  //Ring color is copper-400
   return (
     <button
       css={[
         tw`px-2 py-1 uppercase border-2 border-copper-300 bl-text-lg`,
         disabled
           ? tw`cursor-not-allowed bg-gray-yellow-300 bg-opacity-60 text-gray-yellow-600 text-opacity-60`
-          : tw`hover:bg-copper-400 hover:text-gray-yellow-100
-          focus:outline-none focus-visible:ring-2 focus-visible:ring-copper-400`,
+          : [
+              tw`hover:bg-copper-400 hover:text-gray-yellow-100
+          focus:outline-none`,
+              css`
+                &.focus-visible {
+                  --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0
+                    var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+                  --tw-ring-shadow: var(--tw-ring-inset) 0 0 0
+                    calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+                  box-shadow: var(--tw-ring-offset-shadow),
+                    var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
+                  --tw-ring-opacity: 1;
+                  --tw-ring-color: rgba(171, 133, 94, var(--tw-ring-opacity));
+                }
+              `,
+            ],
       ]}
       disabled={disabled}
       {...props}
