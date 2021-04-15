c797360b34eaa9e0edf54ce29795320119707132
Tothinator <dtoth2016@gmail.com>
2021-04-15 15:21:44 -0500

    Added remark-gfm

--- src/components/project/project-information.tsx
+++ src/components/project/project-information.tsx
@@ -1,6 +1,7 @@
 import tw, { css } from 'twin.macro'
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
 
 import { SearchBar } from './index'
 import { PlusIcon, EditIcon } from 'icons'
@@ -31,7 +32,7 @@ export function ProjectInformation({
         `,
       ]}
     >
-      <div tw="w-9/12 mx-auto space-y-8">
+      <div tw="w-9/12 mx-auto py-10 space-y-8">
         <SearchBar />
         {user?.role === 'ADMIN' && (
           <IconLink pathName={`/project/${projectId}/#`}>
@@ -56,7 +57,7 @@ export function ProjectInformation({
                   {format(new Date(createdAt), 'M/d/yy')}
                 </span>
               </div>
-              <ReactMarkdown>{body}</ReactMarkdown>
+              <ReactMarkdown plugins={[gfm]}>{body}</ReactMarkdown>
             </div>
           ))}
         </div>
