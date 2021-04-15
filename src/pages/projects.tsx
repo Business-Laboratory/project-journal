0dcec8570ca9c8968ff0e09b1917f81b70b9f0a7
tripplybrand <tripplybrand@gmail.com>
2021-04-15 16:43:01 -0400

    style focus ring for emal.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -16,8 +16,9 @@ export default function Projects() {
   const { status, data } = useQuery('projects', fetchProjects)
 
   if (status === 'error') {
+    //Ring color is copper-400
     return (
-      <div tw="space-y-6">
+      <div tw="space-y-6 pt-40">
         <h1 tw="bl-text-3xl text-center text-matisse-red-200 uppercase">
           Unable to load projects
         </h1>
@@ -25,7 +26,22 @@ export default function Projects() {
           <p tw="bl-text-2xl text-center">If the issue continues email</p>
           <a
             href="mailto:help@business-laboratory.com"
-            tw="bl-text-2xl text-copper-300"
+            css={[
+              tw`bl-text-2xl text-copper-300
+                focus:outline-none`,
+              css`
+                &.focus-visible {
+                  --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0
+                    var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+                  --tw-ring-shadow: var(--tw-ring-inset) 0 0 0
+                    calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+                  box-shadow: var(--tw-ring-offset-shadow),
+                    var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
+                  --tw-ring-opacity: 1;
+                  --tw-ring-color: rgba(171, 133, 94, var(--tw-ring-opacity));
+                }
+              `,
+            ]}
           >
             help@business-laboratory.com
           </a>
