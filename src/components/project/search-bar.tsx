16c21069a6a95502c3a5de1517e6236ac0911887
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-04-21 16:40:53 -0500

    Replace input with @reach/combobox

--- src/components/project/search-bar.tsx
+++ src/components/project/search-bar.tsx
@@ -1,22 +1,56 @@
-import tw, { css } from 'twin.macro'
+import 'twin.macro'
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+  ComboboxOptionText,
+} from '@reach/combobox'
+import { SearchIcon } from 'icons'
 
-export function SearchBar() {
+import '@reach/combobox/styles.css'
+
+type SearchBarProps = {
+  id?: string
+}
+export function SearchBar({ id = 'projects-search-bar' }: SearchBarProps) {
   return (
-    <input
-      css={[
-        tw`block w-full py-3 px-8`,
-        tw`border border-gray-yellow-600`,
-        tw`hover:border-2 hover:border-copper-300 focus:border-2 focus:outline-none focus:border-copper-400`,
-        css`
-          &:hover,
-          &:focus {
-            margin: -1px;
-          }
-        `,
-      ]}
-      type="search"
-      id="searchbar"
-      placeholder="Search project updates..."
-    />
+    <Combobox aria-label="search projects">
+      <label
+        htmlFor={id}
+        tw="
+          flex items-center w-full px-8 py-3 bl-text-base text-gray-yellow-600
+          ring-1 ring-inset ring-gray-yellow-600
+          hover:(ring-2 ring-copper-300) focus-within:(ring-2 ring-copper-400)
+        "
+      >
+        <SearchIcon tw="w-5 h-5" />
+        <ComboboxInput
+          id={id}
+          tw="ml-3 w-full placeholder-gray-yellow-300 focus:outline-none"
+          placeholder="Search project updates..."
+        />
+      </label>
+      <ComboboxPopover>
+        <ComboboxList>
+          <ComboboxOption value="Apple">
+            <ComboboxOptionText />
+          </ComboboxOption>
+          <ComboboxOption value="Banana">
+            <ComboboxOptionText />
+          </ComboboxOption>
+          <ComboboxOption value="Orange">
+            <ComboboxOptionText />
+          </ComboboxOption>
+          <ComboboxOption value="Pineapple">
+            <ComboboxOptionText />
+          </ComboboxOption>
+          <ComboboxOption value="Kiwi">
+            <ComboboxOptionText />
+          </ComboboxOption>
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
   )
 }
