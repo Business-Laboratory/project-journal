ba5262f12151665702857a13882929b465ff3f68
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-04-26 12:31:11 -0500

    Remove test component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import type { AppProps } from 'next/app'
 import { GlobalStyles } from 'twin.macro'
 import { QueryClient, QueryClientProvider } from 'react-query'
@@ -6,7 +5,6 @@ import 'focus-visible'
 
 import { AuthProvider } from '@components/auth-context'
 import { AppBar } from '@components/app-bar'
-import { Button } from '@components/button'
 
 import '../styles/globals.css'
 
@@ -26,7 +24,6 @@ function App({ Component, pageProps }: AppProps & ComponentWithPageLayout) {
       <div tw="absolute inset-0 overflow-y-auto overflow-x-hidden bg-gray-yellow-100 text-gray-yellow-600">
         <AuthProvider>
           <AppBar />
-          <TempImageUpload />
           {
             // get a page root if one was set
             Component.PageLayout ? (
@@ -40,85 +37,6 @@ function App({ Component, pageProps }: AppProps & ComponentWithPageLayout) {
         </AuthProvider>
       </div>
     </QueryClientProvider>
-  )
-}
-
-function TempImageUpload() {
-  const [{ selectedImage, status }, setSelectedImage] = useState<{
-    selectedImage: File | null
-    status: 'noData' | 'ready' | 'uploading' | 'successful' | 'error'
-  }>({ selectedImage: null, status: 'noData' })
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    if (selectedImage === null || status === 'uploading') {
-      return
-    }
-
-    setSelectedImage((prev) => ({ ...prev, status: 'uploading' }))
-
-    try {
-      const result = await fetch('/api/generate-upload-blob-token', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          projectId: 1,
-          fileName: selectedImage.name,
-        }),
-      })
-      const { sasUrl } = await result.json()
-
-      // lazy load the image uploader since it's pretty large
-      const { uploadImage } = await import('@utils/upload-image')
-
-      const imageStorageBlobUrl = await uploadImage(sasUrl, selectedImage)
-      await fetch('/api/update-project-image', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          id: 1,
-          imageStorageBlobUrl,
-        }),
-      })
-      // force the projects to re-query so the image changes
-      queryClient.invalidateQueries('projects')
-      setSelectedImage((prev) => ({ ...prev, status: 'successful' }))
-    } catch (error) {
-      setSelectedImage((prev) => ({ ...prev, status: 'error' }))
-      console.error(error)
-    }
-  }
-
-  return (
-    <form tw="flex flex-col w-56 space-y-2 py-4" onSubmit={handleSubmit}>
-      <label htmlFor="img">Select image:</label>
-      <input
-        type="file"
-        id="img"
-        onChange={(e) => {
-          const file = e.currentTarget.files?.[0]
-          if (file) {
-            setSelectedImage({ selectedImage: file, status: 'ready' })
-          }
-        }}
-        name="img"
-        accept="image/*"
-      />
-      <Button
-        type="submit"
-        disabled={
-          status === 'noData' ||
-          status === 'uploading' ||
-          status === 'successful'
-        }
-      >
-        {status === 'uploading'
-          ? 'uploading image...'
-          : status === 'successful'
-          ? 'upload successful'
-          : 'upload image'}
-      </Button>
-    </form>
   )
 }
 
--- src/pages/api/generate-upload-blob-token.ts
+++ src/pages/api/generate-upload-blob-token.ts
@@ -10,6 +10,7 @@ export type UserData = Exclude<
   undefined
 >
 
+// This api will need to be amended to be a little more usable for generating tokens for all sorts of image uploads
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -22,14 +23,14 @@ export default async function handler(
   }
 
   try {
-    const { projectId, fileName } = checkBody(req.body)
-    checkIfProjectExists(projectId)
+    const { id, fileName } = checkBody(req.body)
+    // assuming id is project id for now
+    checkIfProjectExists(id)
 
     // get a SAS url to upload the image. This will also create the container if it does not exist
     const sasUrl = await generateSasUrlForImageUpload(
-      'project-images', // for now we're just added all project images to the same container
-      // `${new Date().toISOString()}_${fileName}`
-      fileName
+      'project-images', // for now we're just adding all project images to the same container
+      `project-${id}-${fileName}`
     )
 
     res.status(200).json({ sasUrl })
@@ -38,35 +39,29 @@ export default async function handler(
   }
 }
 
-function checkBody({
-  projectId,
-  fileName,
-}: {
-  projectId: unknown
-  fileName: unknown
-}) {
+function checkBody({ id, fileName }: { id: unknown; fileName: unknown }) {
   if (
-    (typeof projectId !== 'string' && typeof projectId !== 'number') ||
-    projectId === '' ||
-    Number.isNaN(Number(projectId))
+    (typeof id !== 'string' && typeof id !== 'number') ||
+    id === '' ||
+    Number.isNaN(Number(id))
   ) {
-    throw new Error(`Invalid projectId ${JSON.stringify(projectId)}`)
+    throw new Error(`Invalid id ${JSON.stringify(id)}`)
   }
 
   if (typeof fileName !== 'string' || fileName === '') {
     throw new Error(`Invalid fileName ${JSON.stringify(fileName)}`)
   }
 
-  return { projectId: Number(projectId), fileName }
+  return { id: Number(id), fileName }
 }
 
-async function checkIfProjectExists(projectId: number) {
+async function checkIfProjectExists(id: number) {
   const projects = await prisma.project.count({
     where: {
-      id: projectId,
+      id,
     },
   })
   if (projects !== 1) {
-    throw new Error(`Project with project id of ${projectId} does not exist`)
+    throw new Error(`Project with project id of ${id} does not exist`)
   }
 }
