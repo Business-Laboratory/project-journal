0736e321726c55bdc4a1215148f151ef363dd078
tripplybrand <tripplybrand@gmail.com>
2021-04-28 12:12:27 -0400

    Add CameraIcon. Change plus icons names.

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,21 +1,23 @@
 import LogoIcon from './bl-logo.svg'
-import PlusIcon from './plus.svg'
-import PlusIconSmall from './plus-small.svg'
+import PlusIcon from './plus-icon.svg'
+import PlusSmallIcon from './plus-small-icon.svg'
 import EditIcon from './edit-icon.svg'
 import SearchIcon from './search-icon.svg'
 import SpinnerIcon from './spinner.svg'
 import GearIcon from './gear-icon.svg'
 import UpdateLinkIcon from './update-link-icon.svg'
 import CloseIcon from './x-out.svg'
+import CameraIcon from './camera-icon.svg'
 
 export {
   LogoIcon,
   PlusIcon,
-  PlusIconSmall,
+  PlusSmallIcon,
   EditIcon,
   SearchIcon,
   CloseIcon,
   SpinnerIcon,
   GearIcon,
   UpdateLinkIcon,
+  CameraIcon,
 }
