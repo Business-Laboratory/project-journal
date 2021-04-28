55a7173355d48038f6132daa0b030880f82fe2f6
tripplybrand <tripplybrand@gmail.com>
2021-04-28 16:48:27 -0400

    Create page's basic structure. Add IconLink element.

--- src/pages/admins.tsx
+++ src/pages/admins.tsx
@@ -1,5 +1,23 @@
 // Assign and remove admin users
+import 'twin.macro'
+import Head from 'next/head'
+import { IconLink } from '@components/icon-link'
+import { EditIcon } from 'icons'
 
 export default function DefaultComponent() {
-  return <h1>TODO: Implement this page</h1>
+  return (
+    <>
+      <Head>
+        <title>Admins | Project Journal</title>
+      </Head>
+      <main tw="pt-10 w-9/12 min-w-max mx-auto space-y-8">
+        <IconLink pathName="#">
+          {/*Add fill-copper-300 when I've merged in my icon update */}
+          <EditIcon tw="w-6 h-6" />
+          <span tw="bl-text-3xl">Admins</span>
+        </IconLink>
+        {/* Add grid of admins */}
+      </main>
+    </>
+  )
 }
